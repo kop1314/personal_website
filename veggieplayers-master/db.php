a2312484fb621fc021fa0b191684a371cd1e91fa
Zhouzhou Yang <zy15@illinois.edu>
2021-07-21 18:18:29 -0400

    update db info

--- veggieplayers-master/db.php
+++ veggieplayers-master/db.php
@@ -6,12 +6,3 @@ $user = 'root';
 $pass = '';
 $db = 'veggiebirds_users';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-
-
-/*
-$host = 'sql101.epizy.com';
-$user = 'epiz_28620757';
-$pass = 'kUtH7LVL9I';
-$db = 'epiz_28620757_veggiebirds_users';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-*/
